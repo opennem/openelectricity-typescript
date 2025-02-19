e33e118eb8b777004abc7ce32f1d68608c6ec0b9
Nik Cubrilovic <git@nikcub.me>
2025-02-19 15:34:37 +1100

    build: fix nodejs build path in package and directory and export types from package index

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,42 @@
+/**
+ * OpenElectricity API Client
+ * Main entry point for the OpenElectricity API client library
+ */
+
+// Export the client
 export * from "./client"
 export { OpenElectricityClient as default } from "./client"
+
+// Export all types
+export type {
+  DataInterval,
+  // Metric Types
+  DataMetric,
+  DataPrimaryGrouping,
+  DataSecondaryGrouping,
+  // API Response Types
+  IAPIResponse,
+  // Request Parameter Types
+  IFacilityTimeSeriesParams,
+  IMarketTimeSeriesParams,
+  INetworkTimeSeries,
+  INetworkTimeSeriesParams,
+
+  // Response Types
+  ITimeSeriesResponse,
+  ITimeSeriesResult,
+  IUser,
+  IUserMeta,
+  MarketMetric,
+  Metric,
+  // Network and Data Types
+  NetworkCode,
+  // User Types
+  UserPlan
+} from "./types"
+
+// Export DataTable types
+export type { IDataTableRow, IDescribeResult } from "./datatable"
+
+// Export DataTable class
+export { DataTable } from "./datatable"
